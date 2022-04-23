f2acec29b6d2c49db202095c0a67cd98cda09aad
Krai53n <greasha46@gmail.com>
2022-04-23 19:26:52 +0300

    add checking point in polygon

--- gon/gon.cpp
+++ gon/gon.cpp
@@ -1,26 +1,15 @@
-#include <malloc.h>
-#include <stdio.h>
-
+#include "window.h"
 #include "gon.h"
 
 const float RADIAN = M_PI / 180;
 
-void MegagonDraw(Megagon meg)
+// Transform coords from position at trigonometric circle multpiplied on scale
+// to screen coords
+void transformCoords(int centerx, int centery, float degreepos,
+    float size, int& x, int& y)
 {
-    for (int vert = 0; vert < meg.vertexesNum; vert++)
-    {
-        SDL_RenderDrawLine(
-            ren,
-            cos(meg.vertexes[vert].pos * RADIAN) 
-                * meg.vertexes[vert].size + meg.pos.x,
-            sin(meg.vertexes[vert].pos * RADIAN)
-                * meg.vertexes[vert].size + meg.pos.y,
-            cos(meg.vertexes[(vert+1)%meg.vertexesNum].pos * RADIAN)
-                * meg.vertexes[(vert+1)%meg.vertexesNum].size + meg.pos.x,
-            sin(meg.vertexes[(vert+1)%meg.vertexesNum].pos * RADIAN)
-                * meg.vertexes[(vert+1)%meg.vertexesNum].size + meg.pos.y
-        );
-    }
+    x = cos(degreepos * RADIAN) * size + centerx;
+    y = sin(degreepos * RADIAN) * size + centery;
 }
 
 void MegagonInit(Megagon& meg, SDL_Point pos, int vertexesNum, float size)
@@ -41,4 +30,74 @@ void MegagonInit(Megagon& meg, SDL_Point pos, int vertexesNum, float size)
 void MegagonFree(Megagon meg)
 {
     free(meg.vertexes);
+}
+
+int** getTransformedCoordsFromMegagonToPolygon(const Megagon& meg, int mouse_x, int mouse_y)
+{
+    int** polygon = (int**)malloc(sizeof(int*) * (meg.vertexesNum+1));
+    for (int i = 0; i < meg.vertexesNum + 1; i++)
+    {
+        polygon[i] = (int*)malloc(sizeof(int) * 2);
+
+        int x, y;
+        transformCoords(meg.pos.x, meg.pos.y, meg.vertexes[i%meg.vertexesNum].pos,
+            meg.vertexes[i % meg.vertexesNum].size, x, y);
+        polygon[i][0] = x - mouse_x;
+        polygon[i][1] = y - mouse_y;
+    }
+    return polygon;
+}
+
+void freeTransformedCoordsFromMegagonToPolygon(int** polygon, int vertexes)
+{
+    for (int i = 0; i < vertexes + 1; i++)
+        free(polygon[i]);
+    free(polygon);
+}
+
+float getVectorLen(int* v)
+{
+    return sqrt(v[0]*v[0] + v[1]*v[1]);
+}
+
+float getAngleBetweenVectors(int* v1, int* v2)
+{
+    float cosine = (v1[0]*v2[0] + v1[1]*v2[1]) / (getVectorLen(v1) * getVectorLen(v2));
+    return acos(cosine);
+}
+
+void MegagonDraw(Megagon meg)
+{
+    for (int vertidx = 0; vertidx < meg.vertexesNum; vertidx++)
+    {
+        int vertx1, verty1;
+        int vertx2, verty2;
+        transformCoords(meg.pos.x, meg.pos.y,
+            meg.vertexes[vertidx].pos,
+            meg.vertexes[vertidx].size,
+            vertx1, verty1
+        );
+        transformCoords(meg.pos.x, meg.pos.y,
+            meg.vertexes[(vertidx + 1) % meg.vertexesNum].pos,
+            meg.vertexes[(vertidx + 1) % meg.vertexesNum].size,
+            vertx2, verty2
+        );
+        SDL_RenderDrawLine(ren, vertx1, verty1, vertx2, verty2);
+    }
+}
+
+bool MegagonIsClicked(Megagon meg, int x, int y)
+{
+    int** polygon = getTransformedCoordsFromMegagonToPolygon(meg, x, y);
+    float sumAngles = 0;
+
+    for (int i = 0; i < meg.vertexesNum; i++)
+        sumAngles += getAngleBetweenVectors(polygon[i], polygon[i+1]);
+
+    float diff = 0.000001;
+    if (sumAngles - diff <= M_PI*2 && M_PI*2 <= sumAngles + diff)
+        return true;
+
+    freeTransformedCoordsFromMegagonToPolygon(polygon, meg.vertexesNum);
+    return false;
 }
--- gon/gon.h
+++ gon/gon.h
@@ -1,5 +1,7 @@
 #include <SDL.h>
+#include <math.h>   
 #include <stdio.h>
+#include <malloc.h>
 
 #pragma once
 
@@ -12,11 +14,16 @@ struct MegagonVertex
 struct Megagon
 {
     SDL_Point pos;
+    SDL_Color color;
+
     int vertexesNum;
     MegagonVertex* vertexes = NULL;
     float size;
+    bool selected = false;
 };
 
-void MegagonDraw(Megagon meg);
 void MegagonInit(Megagon& meg, SDL_Point pos, int vertexesNum, float size);
 void MegagonFree(Megagon meg);
+
+void MegagonDraw(Megagon meg);
+bool MegagonIsClicked(Megagon meg, int x, int y);
